2d32075895bd25279348ebe84ca4800a7e817548
rokssoliana <roksasm@gmail.com>
2026-05-04 14:41:18 +0300

    feat(lab9): implement Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TrainList from '../components/TrainList';
+
+const Home = () => {
+  return (
+    <div>
+      <TrainList />
+    </div>
+  );
+};
+
+export default Home;
